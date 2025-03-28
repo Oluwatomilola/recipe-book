8e538bdb755e4cb1bd5794970c0a7472d124a940
ISRAEL ADEWOLE <israelayobami3@gmail.com>
2025-03-28 19:49:12 +0100

    I created and link the home page with other page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Logo from '../assets/logo.png'
+import "./Home.css"
+import { Link } from "react-router";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <img className='logo' src={Logo} alt="Logo"   />
+
+        <ul className="nav">
+
+            <li>Home</li>
+            <li>Recipe-List</li>
+            {/* <li>
+                <Link to="/Home">Home</Link>
+            </li>
+
+            <li>
+                <Link to="/RecipeList">Recipe-List</Link>
+            </li> */}
+        </ul>
+      </div>
+
+      <div className='About'>
+        <h2>About</h2>
+        <p className="intro">Welcome to our Recipe Book app, your ultimate culinary companion! Whether you're a seasoned chef or just starting your cooking journey, our app offers a wide variety of delicious, easy-to-follow recipes for every taste and occasion. From quick weeknight dinners to indulgent desserts, discover step-by-step instructions, helpful tips, and handy features like personalized favorites and shopping lists. Explore new flavors, try exciting dishes, and transform your kitchen into a gourmet haven—one recipe at a time!</p>
+        <button className='search'>Search</button>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Home
