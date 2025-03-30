eaba645993678dec1aa00418bf71ebce83302a2b
Davedigital <agbooladavid001@outlook.com>
2025-03-30 11:39:02 +0100

    Installed react router dom to establish link from home page to recipe list. Changed the file extention from recipe.js to Recipe.jsx

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect }from 'react';
-import Recipe from './recipe';
+import Recipe from './Recipe';
 
 const RecipeList = () => {
   //Initialize state to store recipes
